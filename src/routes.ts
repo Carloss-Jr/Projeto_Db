f47d2e389860df59cb2d75397941deb34c59a7dc
Carlos Jr <carlos.gluckk@gmail.com>
2021-09-08 16:44:22 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import CadastroController from './controllers/CadastroController'
+
+const routes = Router()
+
+routes.post('/cadastros', CadastroController.create)
+
+routes.put('/cadastros/:id', CadastroController.update)
+
+routes.get('/cadastros', CadastroController.list)
+
+routes.get('/cadastros/:id', CadastroController.find)
+
+routes.delete('/cadastros/:id', CadastroController.delete)
+
+
+
+export default routes
